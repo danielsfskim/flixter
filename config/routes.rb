5f31a65335ff3192a4fee8bc0671d4b3620a5e97
Daniel <danielsfskim@gmail.com>
2017-04-28 23:06:46 +0000

    Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show] do
+      # this is the line that is important
       resources :sections, only: [:new, :create]
     end
   end
