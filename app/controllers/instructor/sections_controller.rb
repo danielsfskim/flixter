1f515a728a28bcb6b427deb68b0bfa0cd764802f
Daniel <danielsfskim@gmail.com>
2017-05-11 01:28:16 +0000

    challenge section

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -11,7 +12,21 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render json: {
+      success: true
+    }
+  end
+
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -19,10 +34,18 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
   end
+end
 
   def section_params
     params.require(:section).permit(:title)
