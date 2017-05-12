e95bfb052c992ccccbb1c94c920c16303c229eeb
Daniel <danielsfskim@gmail.com>
2017-05-12 19:08:45 +0000

    next lesson button fixes

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
@@ -8,7 +8,6 @@ class LessonsController < ApplicationController
   private
 
   helper_method :current_lesson
-  
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
